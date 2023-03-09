5b0f6848677ccd540687b0e7121c594fd69ef225
peixed351 <peixed351@gmail.com>
2023-03-09 12:10:54 +0300

    update project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import os
 
 import supervisely as sly
-from supervisely.io.json import dump_json_file
-from supervisely.io.fs import silent_remove, remove_dir, copy_file
+import json
 from os.path import join
 
 from dotenv import load_dotenv
@@ -16,17 +15,6 @@ STORAGE_DIR = sly.app.get_data_dir()
 ANN_FILE = "labels.json"
 
 
-def download_project(api, project_name, project_id, dataset_id):
-    # check if dataset id is specified
-    dataset_ids = [dataset_id] if dataset_id is not None else None
-
-    # make project directory path and download project
-    project_dir = join(STORAGE_DIR, f"{project_id}_{project_name}")
-    sly.download_project(api, project_id, project_dir, dataset_ids=dataset_ids, log_progress=True)
-    sly.logger.info(f"Project: '{project_name}' has been successfully downloaded.")
-    return project_dir
-
-
 class MyExport(sly.app.Export):
     def process(self, context: sly.app.Export.Context):
         # create api object to communicate with Supervisely Server
@@ -35,34 +23,40 @@ class MyExport(sly.app.Export):
         # get project info from server
         project_info = api.project.get_info_by_id(id=context.project_id)
 
-        # download project
-        project_dir = download_project(api, project_info.name, project_info.id, context.dataset_id)
+        # make project directory path
+        data_dir = join(STORAGE_DIR, f"{project_info.id}_{project_info.name}")
 
-        # read local Supervisely project
-        project = sly.Project(directory=project_dir, mode=sly.OpenMode.READ)
+        # get project meta
+        meta_json = api.project.get_meta(id=context.project_id)
+        project_meta = sly.ProjectMeta.from_json(meta_json)
+
+        # get datasets info from server
+        dataset_infos = api.dataset.get_list(project_id=context.project_id)
 
         # iterate over datasets in project
-        for dataset in project:
+        for dataset in dataset_infos:
             result_anns = {}
 
             # create Progress object to track progress
             ds_progress = sly.Progress(
                 f"Processing dataset: '{dataset.name}'",
-                total_cnt=len(dataset),
+                total_cnt=dataset.images_count,
             )
+
+            # get dataset images info
+            images = api.image.get_list(dataset.id)
+
             # iterate over images in dataset
-            for item_name in dataset:
+            for image in images:
                 labels = []
 
-                # get image and annotation path for item
-                img_path, ann_path = dataset.get_item_paths(item_name)
-
-                # copy image to new path for export in new format
-                new_item_path = join(project_dir, dataset.name, item_name)
-                copy_file(img_path, new_item_path)
+                # create path for eaach image and download it from server
+                image_path = join(data_dir, dataset.name, image.name)
+                api.image.download(image.id, image_path)
 
                 # download annotation for current image
-                ann = sly.Annotation.load_json_file(ann_path, project.meta)
+                ann_json = api.annotation.download_json(image.id)
+                ann = sly.Annotation.from_json(ann_json, project_meta)
 
                 # iterate over labels in current annotation
                 for label in ann.labels:
@@ -83,28 +77,17 @@ class MyExport(sly.app.Export):
                         }
                     )
 
-                result_anns[item_name] = labels
+                result_anns[image.name] = labels
+
+                # increment the current progress counter by 1
+                ds_progress.iter_done_report()
 
             # create JSON annotation in new format
-            dump_json_file(result_anns, join(dataset.directory, ANN_FILE), indent=2)
+            filename = join(data_dir, dataset.name, ANN_FILE)
+            with open(filename, "w") as fout:
+                json.dump(result_anns, fout, indent=2)
 
-            # remove 'img' and 'ann' folders in Supervisely format
-            remove_dir(dataset.img_dir)
-            remove_dir(dataset.ann_dir)
-
-            # increment the current progress counter by 1
-            ds_progress.iter_done_report()
-
-        # remove project meta file in Supervisely format from project
-        silent_remove(join(project_dir, "meta.json"))
-
-        # archive project directory and return path to archive
-        # sly.app.Export will use it to upload result in Team Files
-        archive_name = f"{project_info.id}_{project_info.name}.tar"
-        archive_path = join(STORAGE_DIR, archive_name)
-        sly.fs.archive_directory(project_dir, archive_path)
-        sly.logger.info("Result directory is archived")
-        return archive_path
+        return data_dir
 
 
 app = MyExport()
